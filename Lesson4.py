588e0b41606fea539c5ecd2615b0ec555b5b2f5d
IlyaKurskoj <86369293+IlyaKurskoj@users.noreply.github.com>
2021-06-23 22:07:23 +0300

    Initial commit

---
+++ Lesson4.py
@@ -0,0 +1,37 @@
+Name="Илья"
+Last_name="Курской"
+Sex="мужской"
+Age="25"
+weight=63
+height=1.78
+bmi=weight/height**2
+sentence="Hi! my name is "+Name+" "+Last_name+','+"I'm "+Age+"years old. Мой вес "+str(weight)+ \
+         'кг, а рост '+str(height)+'метров. Мой индекс массы тела равен'+str(bmi)
+
+print(sentence)
+
+sentence_two="""
+        Baa, baa, black sheep,
+
+Have you any wool?
+
+Yes sir, yes sir,
+
+Three bags full
+
+
+One for the master,\nOne for the dame,
+
+And one for the little boy
+
+Who lives down the lane
+
+
+Baa, baa, black sheep,
+
+Have you any wool?
+
+Yes sir, yes sir,
+
+Three bags full"""
+print(sentence_two)
